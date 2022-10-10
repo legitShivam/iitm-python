2e4fae870600867f90b75bd0d7b8581775e24738
Shivam Shandilya <70915754+legitShivam@users.noreply.github.com>
2022-10-10 13:38:40 +0530

    Create grpa3.py

---
+++ week-4/grpa3.py
@@ -0,0 +1,13 @@
+"""
+Accept two square matrices AA and BB of dimensions n \times nn×n as input and compute their product ABAB.
+
+The first line of the input will contain the integer nn. This is followed by 2n2n lines. Out of these, each of the first nn lines is a sequence of comma-separated integers that denotes one row of the matrix AA. Each of the last nn lines is a sequence of comma-separated integers that denotes one row of the matrix BB.
+
+Your output should again be a sequence of nn lines, where each line is a sequence of comma-separated integers that denotes a row of the matrix ABAB.
+"""
+
+
+
+
+# Public Test cases passed: 2/2
+# Private Test cases passed: 5/5
